1cdc8fb1dd680914ef4d8f0002597a104dad5c6b
alexcleveland <alexcleveland5@gmail.com>
2026-01-28 19:44:25 -0700

    Final Grid Layout

--- main.py
+++ main.py
@@ -27,16 +27,19 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.grid(row=1, column=1)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 50, "bold"))
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 
 title_label =Label(window, text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 title_label.grid(row=0, column=1)
 
-start_button = Button(text="Start", bg=YELLOW, fg=GREEN)
+start_button = Button(text="Start", bg=YELLOW, fg=GREEN, highlightthickness=0)
 start_button.grid(row=2, column=0)
 
-restart_button = Button(text="Restart", bg=YELLOW, fg=GREEN)
+restart_button = Button(text="Restart", bg=YELLOW, fg=GREEN, highlightthickness=0)
 restart_button.grid(row=2, column=2)
+
+check_marks = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"))
+check_marks.grid(row=3, column=1)
 
 
 
